c6fe46a96901e358651f7aca0a993661294d03b7
kimjunu07 <kimjunu07@snu.ac.kr>
2025-04-29 17:52:50 +0900

    Create skeleton.py
    Signed-off-by: kimjunu07 <kimjunu07@snu.ac.kr>

---
+++ skeleton.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Header
+import numpy as np  # 이미지 평균 계산을 위해 필요
+
+class DetermineColor(Node):
+    def __init__(self):
+        super().__init__('color_detector')
+        self.image_sub = self.create_subscription(Image, '/color', self.callback, 10)
+        self.color_pub = self.create_publisher(Header, '/rotate_cmd', 10)
+        self.bridge = CvBridge()
+
+    def callback(self, data):
+        try:
+            # listen image topic
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+
+            # prepare rotate_cmd msg
+            # DO NOT DELETE THE BELOW THREE LINES!
+            msg = Header()
+            msg = data.header
+            msg.frame_id = '0'  # default: STOP
+
+            # determine background color
+            avg_color = np.mean(image, axis=(0, 1))  # 평균 BGR 값 계산
+            blue, green, red = avg_color
+
+            if blue > red and blue > green:
+                msg.frame_id = '-1'  # CW (파란색)
+            elif red > blue and red > green:
+                msg.frame_id = '+1'  # CCW (빨간색)
+            else:
+                msg.frame_id = '0'   # STOP (그 외)
+
+            # publish color_state
+            self.color_pub.publish(msg)
+
+        except CvBridgeError as e:
+            self.get_logger().error('Failed to convert image: %s' % e)
+
+
+if __name__ == '__main__':
+    rclpy.init()
+    detector = DetermineColor()
+    rclpy.spin(detector)
+    detector.destroy_node()
+    rclpy.shutdown()
